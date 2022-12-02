e4af738c242910e0ce38f19fabc53e1a32a7799c
alimarangoz <ali.marangoz1999@gmail.com>
2022-12-02 19:30:44 +0300

    Creates user in Realtime database and user recycler view adapter gets the users info from database.

--- app/src/main/java/com/example/chatapplication/User.kt
+++ app/src/main/java/com/example/chatapplication/User.kt
@@ -2,13 +2,15 @@ package com.example.chatapplication
 
 class User {
     var name: String? = null
+    var lastName: String? = null
     var email: String? = null
     var uid: String? = null
 
     constructor(){}
 
-    constructor(name: String?, email: String?, uid: String?){
+    constructor(name: String?, lastName: String? ,email: String?, uid: String?){
         this.name = name
+        this.lastName = lastName
         this.email = email
         this.uid = uid
     }
